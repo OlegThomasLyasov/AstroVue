311b246761ad5e5d2716de6dd06e0cb47b87078a
lyasovov <o.lyasov@modulbank.ru>
2022-11-24 12:58:49 +0500

    up components

---
+++ src/components/imageText.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+div(:class="$style.wrapper" v-once)
+    Text(:data="data")
+    div(:class="$style['image-wrap']")
+        Img(:options="data.image" :source="data.source")
+</template>
+
+<script lang="ts">
+import Text from './common/Text.vue';
+import Img from './common/Img.vue';
+
+export default {
+  props: {  
+    data: {
+      type: Object,
+      required: true
+    },
+  },
+  components:{
+    Text,
+    Img
+  },
+}
+</script>
+
+<style lang="stylus" module>
+.wrapper
+    padding 0 222px
+    display flex
+    flex-direction row
+    +mediaQuery1024()
+      padding 0 30px
+    +mediaQuery1440()
+      padding 0 120px
+.image-wrap
+    margin auto auto
+
+</style>
